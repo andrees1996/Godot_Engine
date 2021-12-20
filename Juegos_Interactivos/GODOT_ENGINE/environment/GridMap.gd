daadc717b188646c21d3c980f6cf1368a36d263c
Andres Vargas <stalin.vargas@epn.edu.ec>
2021-12-20 07:23:14 -0500

    Update/Archivos

---
+++ Juegos_Interactivos/GODOT_ENGINE/environment/GridMap.gd
@@ -0,0 +1,6 @@
+extends GridMap
+
+
+
+func _ready():
+	pass
---
+++ Juegos_Interactivos/GODOT_ENGINE/player/Ball.gd
@@ -0,0 +1,30 @@
+extends RigidBody
+
+export var rolling_force = 40
+export var jump_impulse = 1000
+
+func _ready():
+	$CameraRig.set_as_toplevel(true)
+	$RayCast.set_as_toplevel(true)
+	
+	
+func _physics_process(delta):
+	var old_camera_pos = $CameraRig.global_transform.origin
+	$CameraRig.global_transform.origin = global_transform.origin
+	var ball_pos = global_transform.origin
+	var new_camera_pos = lerp(old_camera_pos, ball_pos, 0.1)
+	$CameraRig.global_transform.origin= new_camera_pos
+	$RayCast.global_transform.origin = global_transform.origin
+	
+	if Input.is_action_pressed("forward"):
+		angular_velocity.x -= rolling_force*delta
+	if Input.is_action_pressed("back"):
+		angular_velocity.x += rolling_force*delta
+	if Input.is_action_pressed("left"):
+		angular_velocity.z += rolling_force*delta
+	if Input.is_action_pressed("right"):
+		angular_velocity.z -= rolling_force*delta
+		
+	var is_on_floor = $RayCast.is_colliding()
+	if Input.is_action_just_pressed("jump") and is_on_floor:
+		apply_central_impulse(Vector3.UP*jump_impulse)
